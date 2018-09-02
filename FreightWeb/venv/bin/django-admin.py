ca57e4e284cb1cf77eb98713508ec512b69bc76f
韦春孟 <963431755@qq.com>
2018-09-02 17:47:38 +0800

    first

---
+++ FreightWeb/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/weichunmeng/Documents/my_work_2018_9_2/FreightWeb/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
