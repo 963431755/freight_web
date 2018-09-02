68ad51f03491449a8aff3f823cf2e4dad7181d03
韦春孟 <963431755@qq.com>
2018-09-02 20:49:58 +0800

    add restframwork

--- FreightWeb/FreightWeb/urls.py
+++ FreightWeb/FreightWeb/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import url, include
+from account import urls as account_urls
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from account import views as account_views
+
+
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', account_views.UserViewSet)
+router.register(r'groups', account_views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    # path('^', include(router.urls)),
+    # path('^api-auth/', include('rest_framework.urls'))
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
